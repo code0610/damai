195e1cc852acb2366fffd73814d1dd586800d4f8
WangFan <292335860@qq.com>
2018-09-02 12:46:29 +0800

    增加戏剧页

---
+++ src/pages/xijv/index.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <div class="xijv">
+            <Header-top></Header-top>
+            <Header-search></Header-search>
+            <Tab-and-banner></Tab-and-banner>
+           
+    </div>
+    
+</template>
+
+
+<script>
+    import HeaderTop from "@/components/HeadTop";
+    import HeaderSearch from "@/components/HeadSearch";
+    import TabAndBanner from "./TabAndBanner";
+	
+
+    export default {
+        name:"xijv",
+        data(){
+            return{
+
+            }
+        },
+        components:{
+            HeaderTop,
+            HeaderSearch,
+            TabAndBanner
+         
+
+            
+        }
+    }
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Admin from '@/pages/Admin'
 import OrderMenu from '@/components/Order/OrderMenu'
 import MyMsg from "@/components/Order/MyMsg"
 import Index from "@/pages/Index"
+import xijv from "@/pages/xijv"
 
 Vue.use(Router)
 
@@ -33,6 +34,11 @@ export default new Router({
         name: 'MyMsg',
         component: MyMsg
       }]
+    },
+    {
+      path:'/xijv',
+      name:"xijv",
+      component:xijv
     }
   ]
 })
