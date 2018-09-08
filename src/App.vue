4abf6921e90b0ff84ab42bf92ce085f98b48b5fa
limeixue <limeixue1205@163.comgit config --global user.email limeixue1205@163.com>
2018-09-08 16:07:36 +0800

    新增的css样式：右浮动 li去圆点 线性渐变

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,6 @@
 <template>
   <div id="app">
     <!--<img src="./assets/logo.png">-->
-      <div>
-        <div class="admin_cont_menu floatLeft">
-          <div class="line-height">我的大麦</div>
-          <div>
-            <ul>
-              <li>
-                <div class="line-height">交易中心</div>
-                <ul>
-                  <li class="line-height text-indent OrderManagement"><router-link to="/admin/ordermenu">订单管理</router-link></li>
-                </ul>
-              </li>
-              <li>
-                <div class="line-height">账户中心</div>
-                <ul>
-                  <li class="line-height text-indent"><router-link to="/admin/mymsg">个人信息</router-link></li>
-                  <li class="line-height text-indent"><router-link to="/admin/purchasers">常用购票人</router-link></li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="admin_cont_c floatRight">
-          <router-view />
-        </div>
-      </div>
     <router-view/>
   </div>
 </template>
